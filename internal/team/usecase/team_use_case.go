e97bd03ea9b1cf8843ec03a31be0f08ff20fd175
Alim Ikegami <alimikegami1@gmail.com>
2022-05-12 13:20:46 +0800

    feat: add get team details feature

--- internal/team/usecase/team_use_case.go
+++ internal/team/usecase/team_use_case.go
@@ -25,3 +25,27 @@ func (tuc *TeamUseCase) CreateTeam(userID uint, team dto.TeamRequest) error {
 	err := tuc.tr.CreateTeam(teamEntity)
 	return err
 }
+
+func (tuc *TeamUseCase) GetTeamDetailsByID(teamID uint) (dto.TeamDetailsResponse, error) {
+	team, err := tuc.tr.GetTeamByID(teamID)
+
+	if err != nil {
+		return dto.TeamDetailsResponse{}, err
+	}
+
+	teamDetails := dto.TeamDetailsResponse{
+		Name:        team.Name,
+		Description: team.Description,
+		Capacity:    team.Capacity,
+	}
+
+	members, err := tuc.tr.GetTeamMembersByID(teamID)
+	for _, member := range members {
+		teamDetails.TeamMembers = append(teamDetails.TeamMembers, dto.TeamMemberResponse{
+			UserID: member.ID,
+			Name:   member.User.Name,
+		})
+	}
+
+	return teamDetails, nil
+}
