64195abae117d777be97e4eef48162de871ba6f8
Alim Ikegami <alimikegami1@gmail.com>
2022-05-13 10:16:33 +0800

    feat: add get competition registrations feature

--- internal/competition/dto/competition_registration_response.go
+++ internal/competition/dto/competition_registration_response.go
@@ -1,9 +1,20 @@
 package dto
 
 type TeamCompetitionRegistrationResponse struct {
-	ID            uint
-	TeamID        uint
-	TeamName      string
-	CompetitionID uint
-	IsAccepted    uint
+	ID            uint   `json:"id"`
+	TeamID        uint   `json:"teamID"`
+	TeamName      string `json:"teamName"`
+	CompetitionID uint   `json:"competitionID"`
+	IsAccepted    uint   `json:"isAccepted"`
+}
+
+type IndividualCompetitionRegistrationResponse struct {
+	ID                uint   `json:"id"`
+	UserID            uint   `json:"userID"`
+	UserName          string `json:"userName"`
+	Email             string `json:"email"`
+	PhoneNumber       string `json:"phoneNumber"`
+	SchoolInstitution string `json:"schoolInstitution"`
+	CompetitionID     uint   `json:"competitionID"`
+	IsAccepted        uint   `json:"isAccepted"`
 }
--- internal/competition/repository/competition_repository.go
+++ internal/competition/repository/competition_repository.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/alimikegami/compnouron/internal/competition/entity"
 	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
 )
 
 type CompetitionRepository struct {
@@ -32,11 +33,15 @@ func (cr *CompetitionRepository) DeleteCompetition(ID uint) error {
 	return nil
 }
 
-func (cr *CompetitionRepository) GetCompetitionByID(ID uint) *entity.Competition {
+func (cr *CompetitionRepository) GetCompetitionByID(ID uint) (entity.Competition, error) {
 	var competition entity.Competition
-	cr.db.First(&competition, ID)
+	result := cr.db.First(&competition, ID)
 
-	return &competition
+	if result.Error != nil {
+		return entity.Competition{}, nil
+	}
+
+	return competition, nil
 }
 
 func (cr *CompetitionRepository) UpdateCompetition(competition entity.Competition) error {
@@ -57,16 +62,16 @@ func (cr *CompetitionRepository) Register(competitionRegistration entity.Competi
 	return nil
 }
 
-// func (cr *CompetitionRepository) GetCompetitionRegistration(competitionID uint) ([]entity.CompetitionRegistration, error) {
-// 	var competitionRegistration []entity.CompetitionRegistration
+func (cr *CompetitionRepository) GetCompetitionRegistration(competitionID uint) ([]entity.CompetitionRegistration, error) {
+	var competitionRegistration []entity.CompetitionRegistration
 
-// 	result := cr.db.Preload(clause.Associations).Where("competition_id = ?", competitionID).Find(&competitionRegistration)
-// 	if result.Error != nil {
-// 		return []entity.CompetitionRegistration{}, result.Error
-// 	}
+	result := cr.db.Preload(clause.Associations).Where("competition_id = ?", competitionID).Find(&competitionRegistration)
+	if result.Error != nil {
+		return []entity.CompetitionRegistration{}, result.Error
+	}
 
-// 	return competitionRegistration, nil
-// }
+	return competitionRegistration, nil
+}
 
 func (cr *CompetitionRepository) RejectCompetitionRegistration(id uint) error {
 	result := cr.db.Model(&entity.CompetitionRegistration{}).Where("id = ?", id).Update("is_accepted", 0)
