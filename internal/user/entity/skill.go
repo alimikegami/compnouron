918e60c68a3392456625169bd3717740e9736542
Alim Ikegami <alimikegami1@gmail.com>
2022-05-20 16:34:57 +0800

    feat: add error skills

--- internal/user/entity/skill.go
+++ internal/user/entity/skill.go
@@ -3,9 +3,9 @@ package entity
 import "time"
 
 type Skill struct {
-	ID        uint `gorm:"primaryKey"`
-	Name      string
-	UserID    uint
+	ID        uint   `gorm:"primaryKey"`
+	Name      string `gorm:"not null;"`
+	UserID    uint   `gorm:"not null"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
 	User      User
